18577178cb8a8a5117478d4dcb00465caf0ab67d
Nathan Skibicki <106558852+NathanSkibicki@users.noreply.github.com>
2024-03-11 16:20:37 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(cors(
     {
-        origin: ["https://tutoring-signup-website.vercel.app"],
+        origin: [""],
         methods: ["POST", "GET"],
         credentials: true
     }
